062509246a5dff7dec4da5345af1f22bfab436f3
huashou.yang@smartcoach.cn <huashou.yang@smartcoach.cn>
2017-01-06 16:39:39 +0800

    Initial Commit

---
+++ PictureScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PictureScrollView
+//
+//  Created by AlexYang on 17/1/6.
+//  Copyright © 2017年 AlexYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PictureScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PictureScrollView
+//
+//  Created by AlexYang on 17/1/6.
+//  Copyright © 2017年 AlexYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
